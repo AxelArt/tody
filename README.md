0bcd91a9a65ef92bfdb5d7a3a3463dbdfba4e67e
Amine Hattak <hattakaminegi@gmail.com>
2023-09-14 02:12:51 +0200

    Committing my local changes to README.md

--- README.md
+++ README.md
@@ -1,16 +1,32 @@
+# TODY
+
 A tool for object detection using YOLO models
 
+## Preliminary steps
+
 Clone the project
+
+```commandline
 git clone https://github.com/AxelArt/tody.git
+```
 
 Run the following commands on your Colab notebook
+
+```commandline
 git submodule init
 git submodule update
+```
 
-Install dependenciesfor YOLOv5
+Install dependencies for YOLOv5
+
+```commandline
 pip install -qr models/yolov5/requirements.txt roboflow 
+```
 
-Run the train using YOLOv5
-python yolo_manager.py --version v5 --mode train --dataset /content/tody/yolov5/data/coco.yaml
+Start training process using YOLOv5
+
+```commandline
+python yolo_manager.py --version v5 --mode train --dataset /content/tody/yolov5/data/coco.yaml --epochs 30 --img 640
+```
 
  
--- yolo_manager.py
+++ yolo_manager.py
@@ -11,6 +11,12 @@ parser.add_argument('--mode', type=str, choices=['train', 'test', 'validation'],
                     help='The mode to run')
 parser.add_argument('--dataset', type=str, required=True,
                     help='The path to the custom dataset')
+parser.add_argument('--img', type=int, required=True,
+                    help='Image size')
+parser.add_argument('--batch', type=int, required=True,
+                    help='The batch size')
+parser.add_argument('--epochs', type=int, required=True,
+                    help='Number of epochs')
 
 # Parse the command-line arguments
 args = parser.parse_args()
@@ -18,11 +24,18 @@ args = parser.parse_args()
 # Define the YOLOv5 and YOLOv6 commands
 if args.version == 'v5':
     if args.mode == 'train':
-        command = f"python models/yolov5/train.py --img 640 --batch 16 --epochs 50 --data {args.dataset} --weights models/yolov5/yolov5s.pt --cache"
+        command = f"python models/yolov5/train.py --img {args.img} --batch {args.batch} --epochs {args.epochs} --data {args.dataset} --weights models/yolov5/yolov5s.pt --cache"
     elif args.mode == 'test':
-        command = f"python models/yolov5/detect.py --weights models/yolov5/runs/train/exp/weights/best.pt --source {args.dataset}/test/images  --img 640 --conf 0.4"
+        command = f"python models/yolov5/detect.py --weights models/yolov5/runs/train/exp/weights/best.pt --source {args.dataset}/test/images  --img {args.img} --conf 0.4"
     else:
-        command = f"python models/yolov5/val.py --weights models/yolov5/runs/train/exp/weights/best.pt --data {args.dataset} --imgsz 640 --task val"
+        command = f"python models/yolov5/val.py --weights models/yolov5/runs/train/exp/weights/best.pt --data {args.dataset} --imgsz {args.img} --task val"
+elif args.version == 'v6':
+    if args.mode == 'train':
+        command = f"python models/yolov6/tools/train.py --img-size {args.img} --batch {args.batch} --epochs {args.epochs} --data {args.dataset} --weights models/yolov6/yolov5s.pt --cache"
+    elif args.mode == 'test':
+        command = f"python models/yolov6/tools/infer.py --weights models/yolov6/runs/train/exp/weights/best.pt --source {args.dataset}/images/test  --img-size {args.img} --conf 0.4"
+    else:
+        command = f"python models/yolov6/tools/eval.py --weights models/yolov6/runs/train/exp/weights/best.pt --data {args.dataset} --img-size {args.img} --task val"        
 else:
     print("Not yet implemented")
 
